273f8f9507bea1e42b9fca9d424704364ba1179d
Julio <Julio@DESKTOP-CKB3TL0>
2025-10-01 12:17:31 -0600

    Homework solution with For cycle

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,29 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("Write your name, user # " + i + ":");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Write your Surname:");
+                string surname = Console.ReadLine();
+
+                Console.WriteLine("Enter your age:");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your weight in kg:");
+                double weight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Enter your height in cm:");
+                double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double BMI = weight / Math.Pow(height / 100, 2);
+
+                Console.WriteLine(name + " " + surname + " is " + age +
+                    " years old, his/her weight is " + weight + " and his/her height is " + height + " cm.");
+                Console.WriteLine("His/her BMI is " + BMI + ".");
+            }
         }
     }
 }
